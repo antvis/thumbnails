202c8b490fa734281798f18ad2b0c7488f87cc75
neoddish <neoddish@outlook.com>
2021-01-12 19:08:27 +0800

    feat: upgrade Thumbnail component & revert expoting (#13)
    * feat: upgrade Thumbnail component & revert expoting
    
    * fix: importing
    
    * fix: import from index

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-
-import { Thumbnails, Thumbnail } from '../src';
+import Thumbnails, { Thumbnail, BAR_CHART } from '../src';
 
 class App extends React.Component<{}> {
   constructor(props: {}) {
@@ -9,16 +8,31 @@ class App extends React.Component<{}> {
 
   public render() {
     return (
-      <div className="symbols">
-        {Object.keys(Thumbnails).map(chart => {
-          const { svgCode, name } = Thumbnails[chart];
-          return (
-            <div className="symbol-img-container">
-              <Thumbnail svg={svgCode} alt={name} width={200} />
-            </div>
-          );
-        })}
-      </div>
+      <>
+        <code>{`<Thumbnail svg={svgCode} alt={name} width={200} />`}</code>
+        <div className="symbols">
+          {Object.keys(Thumbnails).map(chart => {
+            const { svgCode, name } = Thumbnails[chart];
+            return (
+              <div className="symbol-img-container">
+                <Thumbnail svg={svgCode} alt={name} width={200} />
+              </div>
+            );
+          })}
+        </div>
+
+        <br />
+
+        <code>{`<Thumbnail chart={'pie_chart'} alt={'pie'} width={200} />`}</code>
+        <div className="symbol-img-container">
+          <Thumbnail chart={'pie_chart'} alt={'pie'} width={200} />
+        </div>
+
+        <code>{`<Thumbnail svg={BAR_CHART.svgCode} alt={'pie'} width={200} />`}</code>
+        <div className="symbol-img-container">
+          <Thumbnail svg={BAR_CHART.svgCode} alt={'pie'} width={200} />
+        </div>
+      </>
     );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,7 @@
-// generated file index.ts
+import Thumbnails from './thumbnails';
+export default Thumbnails;
 
-import { ChartID } from '@antv/knowledge';
-import { ChartImageInfo } from './interfaces';
-
-import AREA_CHART from './charts/area_chart';
-import BAR_CHART from './charts/bar_chart';
-import BUBBLE_CHART from './charts/bubble_chart';
-import COLUMN_CHART from './charts/column_chart';
-import DONUT_CHART from './charts/donut_chart';
-import GROUPED_BAR_CHART from './charts/grouped_bar_chart';
-import GROUPED_COLUMN_CHART from './charts/grouped_column_chart';
-import HEATMAP from './charts/heatmap';
-import HISTOGRAM from './charts/histogram';
-import LINE_CHART from './charts/line_chart';
-import PERCENT_STACKED_AREA_CHART from './charts/percent_stacked_area_chart';
-import PERCENT_STACKED_BAR_CHART from './charts/percent_stacked_bar_chart';
-import PERCENT_STACKED_COLUMN_CHART from './charts/percent_stacked_column_chart';
-import PIE_CHART from './charts/pie_chart';
-import RADAR_CHART from './charts/radar_chart';
-import SCATTER_PLOT from './charts/scatter_plot';
-import STACKED_AREA_CHART from './charts/stacked_area_chart';
-import STACKED_BAR_CHART from './charts/stacked_bar_chart';
-import STACKED_COLUMN_CHART from './charts/stacked_column_chart';
-
-const Thumbnails: Partial<Record<ChartID, ChartImageInfo>> = {
-  area_chart: AREA_CHART,
-  bar_chart: BAR_CHART,
-  bubble_chart: BUBBLE_CHART,
-  column_chart: COLUMN_CHART,
-  donut_chart: DONUT_CHART,
-  grouped_bar_chart: GROUPED_BAR_CHART,
-  grouped_column_chart: GROUPED_COLUMN_CHART,
-  heatmap: HEATMAP,
-  histogram: HISTOGRAM,
-  line_chart: LINE_CHART,
-  percent_stacked_area_chart: PERCENT_STACKED_AREA_CHART,
-  percent_stacked_bar_chart: PERCENT_STACKED_BAR_CHART,
-  percent_stacked_column_chart: PERCENT_STACKED_COLUMN_CHART,
-  pie_chart: PIE_CHART,
-  radar_chart: RADAR_CHART,
-  scatter_plot: SCATTER_PLOT,
-  stacked_area_chart: STACKED_AREA_CHART,
-  stacked_bar_chart: STACKED_BAR_CHART,
-  stacked_column_chart: STACKED_COLUMN_CHART,
-};
-
-export { Thumbnails };
-
-export {
-  AREA_CHART,
-  BAR_CHART,
-  BUBBLE_CHART,
-  COLUMN_CHART,
-  DONUT_CHART,
-  GROUPED_BAR_CHART,
-  GROUPED_COLUMN_CHART,
-  HEATMAP,
-  HISTOGRAM,
-  LINE_CHART,
-  PERCENT_STACKED_AREA_CHART,
-  PERCENT_STACKED_BAR_CHART,
-  PERCENT_STACKED_COLUMN_CHART,
-  PIE_CHART,
-  RADAR_CHART,
-  SCATTER_PLOT,
-  STACKED_AREA_CHART,
-  STACKED_BAR_CHART,
-  STACKED_COLUMN_CHART,
-};
+export * from './thumbnails';
 
 // react component for displaying Thumbnail images
 export { Thumbnail } from './components/Thumbnail';
