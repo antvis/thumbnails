d8f7fcc6d7dc19de4031bc4c497d678e790e31ba
neoddish <neoddish@outlook.com>
2021-10-28 17:20:01 +0800

    chore: fix one-stop-setup

--- packages/core/src/generated/thumbnails.ts
+++ packages/core/src/generated/thumbnails.ts
@@ -3,19 +3,19 @@
 import { ChartImageInfo } from '../interfaces';
 
 import AREA_CHART from './charts/area_chart';
-import BAR_CHART from './charts/bar_chart';
+import PERCENT_STACKED_AREA_CHART from './charts/percent_stacked_area_chart';
 
 const Thumbnails: Partial<Record<string, ChartImageInfo>> = {
   area_chart: AREA_CHART,
-  bar_chart: BAR_CHART,
+  percent_stacked_area_chart: PERCENT_STACKED_AREA_CHART,
 };
 
 export default Thumbnails;
 
-export const THUMBNAIL_IDS = ['area_chart', 'bar_chart'] as const;
+export const THUMBNAIL_IDS = ['area_chart', 'percent_stacked_area_chart'] as const;
 export type ThumbnailID = typeof THUMBNAIL_IDS[number];
 export function isThumbnailID(id: string): id is ThumbnailID {
   return THUMBNAIL_IDS.includes(id as ThumbnailID);
 }
 
-export { AREA_CHART, BAR_CHART };
+export { AREA_CHART, PERCENT_STACKED_AREA_CHART };
